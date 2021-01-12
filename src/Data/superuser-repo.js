7a2cc55e6818aacd86e668427c42280a8c8cf304
Carlos <arq.camejia@gmail.com>
2021-01-12 14:08:44 -0400

    before working with new branch adding-firebase-storage

--- src/Data/superuser-repo.js
+++ src/Data/superuser-repo.js
@@ -1,44 +1,42 @@
 export default function makeSuperuserRepo(superuserModel) {
-    return Object.freeze({
-        add,
-        exist,
-        findAll,
-        findById,
-        remove,
-        modify,
+  return Object.freeze({
+    add,
+    exist,
+    findAll,
+    findById,
+    remove,
+    modify,
+  });
+
+  async function add(entryUser) {
+    return await superuserModel(entryUser).save();
+  }
+
+  async function exist(id) {
+    return await superuserModel.findOne({
+      $or: [{ email: id }, { username: id }],
     });
+  }
 
+  async function findById(id) {
+    return await superuserModel.findOne({ _id: id });
+  }
 
-    async function add(entryUser) {
-        return await superuserModel(entryUser).save()
-    }
+  async function findAll() {
+    return await superuserModel.find();
+  }
 
-    async function exist(id) {
-        return await superuserModel.findOne({
-            $or: [{ email: id }, { username: id }] 
-        })
-    }
+  async function remove(id) {
+    return await superuserModel.deleteOne({
+      _id: id,
+    });
+  }
 
-    async function findById(id) {
-        return await superuserModel.findOne({ _id: id })
-    }
-
-    async function findAll() {
-        return await superuserModel.find()
-    }
-
-    async function remove(id) {
-        return await superuserModel.deleteOne({
-            _id: id,
-        })
-    }
-
-    async function modify(id, newData) {
-        return await superuserModel.findOneAndUpdate(
-            {_id: id},
-            {$set: newData},
-            {new: true}
-        )
-    }
-
+  async function modify(id, newData) {
+    return await superuserModel.findOneAndUpdate(
+      { _id: id },
+      { $set: newData },
+      { new: true }
+    );
+  }
 }
--- src/Data/user-repo.js
+++ src/Data/user-repo.js
@@ -1,66 +1,63 @@
 export default function makeUserRepo(userModel) {
-    return Object.freeze({
-        add,
-        exist,
-        findById,
-        findAll,
-        remove,
-        removeMany,
-        modify,
+  return Object.freeze({
+    add,
+    exist,
+    findById,
+    findAll,
+    remove,
+    removeMany,
+    modify,
+  });
+
+  async function add(entryUser) {
+    return await userModel(entryUser).save();
+  }
+
+  async function findById(id) {
+    return await userModel.findOne({ _id: id });
+  }
+
+  async function exist(id) {
+    return await userModel.findOne({
+      $or: [{ email: id }, { username: id }],
     });
+  }
 
+  async function findAll(queryParams, filter = {}, sorted = {}) {
+    const { page = 1, limit = 17 } = queryParams;
 
-    async function add(entryUser) {
-        return await userModel(entryUser).save()
-    }
+    const reports = await userModel
+      .find(filter)
+      .limit(limit * 1)
+      .skip((page - 1) * limit)
+      .sort(sorted);
 
-    async function findById(id) {
-        return await userModel.findOne({_id: id})
-    }
+    const count = await userModel.countDocuments(filter);
 
-    async function exist(id) {
+    return {
+      totalOfPages: Math.ceil(count / limit),
+      currentPage: page,
+      reports,
+    };
+  }
 
-        return await userModel.findOne({
-            $or: [{ email: id }, { username: id }] 
-        })
-    }
+  async function remove(id) {
+    return await userModel.deleteOne({
+      _id: id,
+    });
+  }
 
-    async function findAll(queryParams, filter = {}, sorted = {}) {
+  async function removeMany(id) {
+    return await userModel.deleteMany({
+      idCompany: id,
+    });
+  }
 
-        const { page = 1, limit = 17 } = queryParams
-
-        const reports = await userModel.find(filter)
-            .limit(limit * 1)
-            .skip((page - 1) * limit)
-            .sort(sorted)
-
-        const count = await userModel.countDocuments(filter)
-
-        return { 
-            totalOfPages: Math.ceil(count / limit),
-            currentPage: page,
-            reports
-        }
-    }
-
-    async function remove(id) {
-        return await userModel.deleteOne({
-            _id: id,
-        })
-    }
-
-    async function removeMany(id) {
-        return await userModel.deleteMany({
-            idCompany: id,
-        })
-    }
-
-    async function modify(id, newData) {
-        return await userModel.findOneAndUpdate(
-            {_id: id},
-            {$set: newData},
-            {new: true}
-        )
-    }
-
+  async function modify(id, newData) {
+    return await userModel.findOneAndUpdate(
+      { _id: id },
+      { $set: newData },
+      { new: true }
+    );
+  }
 }
